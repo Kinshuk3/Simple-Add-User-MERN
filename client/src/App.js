d9b1f9eaaa309cfdfe5509cb599a59f1ffffa307
Kinshuk3 <64504807+Kinshuk3@users.noreply.github.com>
2023-10-10 00:40:54 -0300

    Added client side code using react to connect to Server and DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUserName] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+
+  const createUserHandler = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name: name,
+      age: age,
+      username: username,
+    }).then((response) => {
+      setListOfUsers((prevState) => {
+        return [...prevState, {name, age, username }];
+      });
+    });
+  };
+
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+
+  const userNameChangeHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
   return (
+    // get the data from DB
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>User: {user.username}</h1>
+            </div>
+          );
+        })}
+
+        {/* post the data to DB*/}
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            onChange={nameChangeHandler}
+          />
+          <input
+            type="number"
+            placeholder="Enter Age"
+            onChange={ageChangeHandler}
+          />
+          <input
+            type="text"
+            placeholder="Enter Username"
+            onChange={userNameChangeHandler}
+          />
+          <button onClick={createUserHandler}>Add User</button>
+        </div>
+      </div>
     </div>
   );
 }
